a38e18dbd1205f8ee89df16459367b603195ba43
fshangala <fshangala@gmail.com>
2026-03-24 18:42:18 +0200

    feat: Add .env file support for server configuration
    - Integrated dotenv package to load environment variables from a .env file on startup.
    - Configured .gitignore to ignore the .env file.
    - Updated the main entry point to use dotenv for host and port configuration.

--- bin/lambo_server.dart
+++ bin/lambo_server.dart
@@ -1,6 +1,12 @@
+import 'package:dotenv/dotenv.dart';
 import 'package:lambo_server/lambo_server_core.dart';
 
 void main() async {
+  var env = DotEnv(includePlatformEnvironment: true)..load();
   final server = LamboServer();
-  await server.start();
+  
+  await server.start(
+    address: env['LAMBO_HOST'],
+    port: int.tryParse(env['LAMBO_PORT'] ?? ''),
+  );
 }
